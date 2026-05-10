4ba981ae0b0fed15ab59d8a26bf3d80c52745fb3
mweb4ik <kiseluk780@gmail.com>
2026-05-10 16:51:42 +0300

    fix dependencies for vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,16 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
+
+  build: {
+    sourcemap: false,
+    minify: 'terser',
+    cssMinify: false   
+  }
 })
